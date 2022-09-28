a57fbd8bbca9319294bbab3924d565c3b95e0bbd
AlexisEspinosaGayosso <Alexis.Espinosa@pawsey.org.au>
2022-09-28 10:39:49 +0800

    Addint tars temporarily and removing garbage
    Removing dirs in the localMatrix folder of fortran
    Creating tars to be transferred to the AMD cluster

--- 5_single-gpu.solutions/fortran/acc-omp.allTests.localMatrix/cleaningTestingIssues/globals.F90
+++
@@ -1,8 +0,0 @@
-       module globals
-       implicit none
-
-       integer, parameter :: GRIDX=2048, GRIDY=2048
-       double precision,allocatable,save :: T(:,:)
-       double precision,allocatable,save :: T_new(:,:)
-
-       end module globals
--- 5_single-gpu.solutions/fortran/acc-omp.allTests.localMatrix/testingIssues/globals.F90
+++
@@ -1,8 +0,0 @@
-       module globals
-       implicit none
-
-       integer, parameter :: GRIDX=2048, GRIDY=2048
-       double precision,allocatable,save :: T_new3(:,:)
-
-
-       end module globals
