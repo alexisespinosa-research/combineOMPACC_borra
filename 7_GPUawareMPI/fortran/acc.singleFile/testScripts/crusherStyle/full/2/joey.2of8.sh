58f232ab6db1701ab45f9f1c4ae466fe7ece00e1
AlexisEspinosaGayosso <Alexis.Espinosa@pawsey.org.au>
2023-03-20 16:54:06 +0800

    Updating joey script

--- 7_GPUawareMPI/fortran/acc.singleFile/testScripts/crusherStyle/full/2/joey.2of8.sh
+++ 7_GPUawareMPI/fortran/acc.singleFile/testScripts/crusherStyle/full/2/joey.2of8.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 #SBATCH --job-name=2of8CrusherLike
-#SBATCH --partition=batch
+#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#--#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=8
 #--#SBATCH --gpus-per-node=8
-#--#SBATCH --gpus-per-task=1
+#SBATCH --gpus-per-task=1
 #SBATCH --time=00:10:00
-#--#SBATCH --exclusive
+#SBATCH --exclusive
 #--#SBATCH --distribution=*:block:block
 #--#SBATCH --mem=60G
 
 #----
 echo "Checking slurm settings with:"
-echo "env | grep SLURM"
-env | grep SLURM
+echo "env | grep SLURM | sort"
+env | grep SLURM | sort
 echo -e "\n\n"
 
 #----
@@ -31,12 +31,6 @@ module list
 echo -e "\n\n"
 
 #----
-exeDir="../../../.."
-exeName=crusher.cray.ftn.laplace_acc.noblock_mpiGPU.twoParallelsBasic.exe
-#exeName=joey.cray.ftn.laplace_acc.noblock_mpiGPU.twoParallelsBasic.exe
-theExe=$exeDir/$exeName
-
-#----
 #Manual CPU binding settings
 #: can't be used in partial requests
 
@@ -46,16 +40,37 @@ export MPICH_GPU_SUPPORT_ENABLED=1
 export OMP_NUM_THREADS=1
 
 #----
+exeDir="../../../.."
+exeName=joey.cray.ftn.laplace_acc.noblock_mpiGPU.twoParallelsBasic.exe
+theExe=$exeDir/$exeName
+
+#----
+#Setting the srun command to test:
+theSrunCmd="srun -l -u -N 1 -n 2 -c 8 --gpus-per-task=1 --gpu-bind=closest --exact"
+#theSrunCmd="srun -l -u -N 1 -n 2 -c 8 --gpu-bind=closest"
+
+#----
 #Execution
 echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
 echo "Running with:"
-echo "srun -l -u -N 1 -n 2 -c 8 --gpu-bind=closest \$theExe 4000"
-srun -l -u -N 1 -n 2 -c 8 --gpu-bind=closest $theExe 4000
+echo "$theSrunCmd \$theExe 4000"
+$theSrunCmd $theExe 4000
 echo -e "\n\n"
 
 #----
-#rocm-smi check:
-srun -n 1 rocm-smi --showhw
+#rocm-smi check
+echo "rocm-smi check using:"
+echo "$theSrunCmd rocm-smi --showhw | sort"
+$theSrunCmd rocm-smi --showhw | sort
+echo -e "\n\n"
+
+#----
+#hello-step check
+helloDir=$MYSOFTWARE/setonix_documentation_material/hello_jobstep
+helloExe=$helloDir/hello_jobstep
+echo "hello-step check using:"
+echo "$theSrunCmd \$helloExe | sort"
+$theSrunCmd $helloExe | sort
 echo -e "\n\n"
 
 #----
